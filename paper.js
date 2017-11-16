20fdf2f837467dbbf92d08439e07a140debdbafd
Nayan R. Yarabolu <nayanyarabolu@ccs.k12.in.us>
2017-11-16 17:18:27 -0500

    ye

--- paper.js
+++ paper.js
@@ -1,5 +1,5 @@
 var fg;
-var tp;
+var fpPossibble;
 
 var ep;
 var epPossible;
@@ -15,23 +15,27 @@ function made(){
 function miss() {
 	attempt += 1;
 }
+
+function fgMade() {
+	fg += 1;
+	fgPossible += 1;
+}
+
+
 function openCity(evt, cityName) {
-    // Declare all variables
+	
     var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
